f8bf17c7d95bab117d3f2e074c6b549ca0d6dc76
Meghal2103 <meghal2103@outlook.com>
2025-02-18 13:59:12 +0530

    day1

---
+++ day1/day1/Controllers/JsonToString.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using FileIO;
+
+namespace day1.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class JsonToString : ControllerBase
+    {
+        [HttpPost(Name = "JsonToString")]
+        public bool Post([FromBody] JsonElement request)
+        {
+            try
+            {
+                FileIO.FileIO.WriteFile(request.ToString());
+                Console.WriteLine(request.ToString());
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ day1/day1/FileIO.cs
@@ -0,0 +1,22 @@
+﻿namespace FileIO
+{
+    public class FileIO
+    {
+        static string FilePath = @"D:\bacancy_training\5) .netcore-daily-task\day1\day1\file.txt";
+
+        public static void WriteFile(string content)
+        {
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(FilePath, true))
+                {
+                    streamWriter.WriteLine(content);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
